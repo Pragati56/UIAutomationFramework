c5d96fcc35ee84a7bd255de7951c6a0dc144a3f6
SinghP90 <pragati.singh@vodafone.com>
2020-05-07 13:28:53 +0200

    changes related to app

--- app/src/androidTest/java/com/example/sampleapplication/TestUIAppwithSeetest.kt
+++ app/src/androidTest/java/com/example/sampleapplication/TestUIAppwithSeetest.kt
@@ -6,6 +6,7 @@ package com.example.sampleapplication
 import android.app.Application
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.util.Log
 
 
@@ -25,8 +26,7 @@ import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.equalTo
 import androidx.test.uiautomator.UiSelector
 import androidx.test.uiautomator.UiObject
-
-
+import org.junit.Assert
 
 
 private const val BASIC_SAMPLE_PACKAGE = "com.example.sampleapplication"
@@ -49,6 +49,12 @@ class TestUIAppwithSeetest{
             Until.hasObject(By.pkg(launcherPackage).depth(0)),
             LAUNCH_TIMEOUT
         )*/
+        val launcherPackage=getLauncherPackageName();
+        Assert.assertThat(launcherPackage, CoreMatchers.notNullValue())
+        device2!!.wait(
+            Until.hasObject(By.pkg(launcherPackage).depth(0)),
+            LAUNCH_TIMEOUT.toLong()
+        )
         // Launch the app
        // val context = ApplicationProvider.getApplicationContext<Context>()
         val context: Context= ApplicationProvider.getApplicationContext()
@@ -60,6 +66,19 @@ class TestUIAppwithSeetest{
             Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
             LAUNCH_TIMEOUT
         )
-        Thread.sleep(50000)
+        Thread.sleep(20000)
+    }
+
+
+    private fun getLauncherPackageName(): String? { // Create launcher Intent
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        // Use PackageManager to get the launcher package name
+        val pm =
+            ApplicationProvider.getApplicationContext<Context>().packageManager
+        val resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        Log.i("intent",resolveInfo.activityInfo.packageName)
+        return resolveInfo.activityInfo.packageName
+
     }
 }
